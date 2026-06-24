91f3a3886dfec089dc589b5de6b3e4d525647690
AnilMande2021 <anilmande91@gmail.com>
2026-06-24 19:58:18 +0530

    first commit

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,11 @@
+import {test,expect} from "@playwright/test";
+
+test("verify page title",async ({page})=>
+{
+    await page.goto("https://www.google.com");
+    let title:string=await page.title();
+    console.log("Title:",title);
+    await expect(page).toHaveTitle("Google");
+
+
+})
